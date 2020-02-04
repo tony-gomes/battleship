3961cfb6f9edb657d1d3166a6290eeb9d3f17c18
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-03 17:38:56 -0700

    Add empty? method to cell and pass test

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,11 +1,20 @@
 class Cell
-  attr_reader :coordinate
+  attr_reader :coordinate, :ship
 
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = ship
   end
 
-  def ship
+  def empty?
+    if @ship == nil
+      true
+    else
+      false
+    end
   end
+
+
+
 
 end
