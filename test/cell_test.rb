5190cf80bfc65acc177db859f8c583b3472850b1
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-03 17:23:18 -0700

    Start building out cell class and tests

---
+++ test/cell_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+
+
+class CellTest < Minitest::Test
+
+  def setup
+    @cell = Cell.new("B4")
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+
+  def test_it_exists
+
+    assert_instance_of Cell, @cell
+  end
+
+  def test_cell_coordinate
+
+    assert_equal "B4", @cell.coordinate
+  end
+
+end
