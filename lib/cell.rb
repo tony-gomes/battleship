709e69267a84af90aa5caa3fbb76e7fd54d666bf
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-03 19:49:40 -0700

    Add cell render method and pass test for all conditions

--- lib/cell.rb
+++ lib/cell.rb
@@ -24,7 +24,30 @@ class Cell
   end
 
   def fire_upon
-    @fired_upon = true
-    @ship.hit
+    if @ship == nil
+      return @fired_upon = true
+    elsif @ship == ship
+      @fired_upon = true
+      @ship.hit
+    end
+  end
+
+  def render(cell_ship_display = false)
+    if @fired_upon == false && cell_ship_display == false
+      print "."
+      return "."
+    elsif @fired_upon == false && @ship == ship && cell_ship_display == true
+      print "S"
+      return "S"
+    elsif @fired_upon == true && @ship == nil
+      print "M"
+      return "M"
+    elsif @fired_upon == true && @ship == ship && @ship.sunk? == false
+      print "H"
+      return "H"
+    elsif @fired_upon == true && @ship.sunk? == true
+      print "X"
+      return "X"
+    end
   end
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -9,6 +9,7 @@ class CellTest < Minitest::Test
   def setup
     @cell = Cell.new("B4")
     @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   def test_it_exists
@@ -46,9 +47,31 @@ class CellTest < Minitest::Test
   def test_cell_fire_upon
     @cell.place_ship(@cruiser)
     @cell.fire_upon
+    cell2 = Cell.new("B4")
+    cell2.fire_upon
 
     assert_equal 2, @cruiser.health
     assert_equal true, @cell.fired_upon?
+    assert_equal true, cell2.fired_upon?
   end
+
+  def test_cell_render
+    cell2 = Cell.new("B4")
+    cell2.fire_upon
+    cell3 = Cell.new("B4")
+    cell3.place_ship(@cruiser)
+    cell3.fire_upon
+    cell4 = Cell.new("C3")
+    cell4.place_ship(@submarine)
+    cell4.fire_upon
+    @submarine.hit
+    
+    assert_equal ".", @cell.render
+    assert_equal "S", @cell.render(true)
+    assert_equal "M", cell2.render
+    assert_equal "H", cell3.render
+    assert_equal "X", cell4.render
+  end
+
 
 end
