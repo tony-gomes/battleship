2e13bc146a48e8129f6eac706b46044e1b8b9065
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-05 15:32:49 -0700

    Update ship test with submarine to be thorough

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -5,6 +5,7 @@ require './lib/ship'
 class ShipTest < Minitest::Test
   def setup
     @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   def test_it_exists_with_attributes_and_health
@@ -12,10 +13,17 @@ class ShipTest < Minitest::Test
     assert_equal "Cruiser", @cruiser.name
     assert_equal 3, @cruiser.length
     assert_equal 3, @cruiser.health
+
+    assert_instance_of Ship, @submarine
+    assert_equal "Submarine", @submarine.name
+    assert_equal 2, @submarine.length
+    assert_equal 2, @submarine.health
   end
 
-  def test_sunk?
+  def test_starts_not_sunk?
     refute @cruiser.sunk?
+
+    refute @submarine.sunk?
   end
 
   def test_it_can_hit_and_sink_ships
@@ -27,5 +35,11 @@ class ShipTest < Minitest::Test
 
     @cruiser.hit
     assert @cruiser.sunk?
+
+    @submarine.hit
+    assert_equal 1, @submarine.health
+
+    @submarine.hit
+    assert @submarine.sunk?
   end
 end
