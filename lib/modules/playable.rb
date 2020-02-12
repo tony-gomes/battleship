f5d272c9f32ea52ea627c17b32408a57464a9f01
Anthony Gomes <code@tonygom.es>
2020-02-11 20:49:26 -0700

    Fix typos left from PR

--- lib/modules/playable.rb
+++ lib/modules/playable.rb
@@ -1,45 +1,6 @@
-<<<<<<< HEAD
 require_relative '../board'
 
 module Playable
-  def render_boards
-    sleep(1.5)
-    puts "\n=============CURRENT COMPUTER BOARD============="
-    puts @computer.battle_board.render
-    puts "\n=============CURRENT USER BOARD================="
-    puts @user.battle_board.render(true)
-  end
-
-  def all_ships_sunk
-    if player_ships.all?(&:sunk?)
-      sleep(1)
-      puts "You lose!\n\n"
-      puts "Would you like to play again?\n\n"
-      start_game
-    end
-      player_shot
-  end
-
-
-
-  # def all_user_ships_sunk
-  #   if @user_ships.all?(&:sunk?)
-  #     sleep(1)
-  #     puts "You lose!\n\n"
-  #     puts "Would you like to play again?\n\n"
-  #     start_game
-  #   elsif @computer_ships.all?(&:sunk?)
-  #     sleep(2)
-  #     puts "You win!\n\n"
-  #     start_game
-  #   else
-  #     player_turn
-  #   end
-  # end
-
-=======
-module Playable
-
   def render_boards
     sleep(1.5)
     puts "\n=============CURRENT COMPUTER BOARD============="
@@ -47,5 +8,4 @@ module Playable
     puts "\n=============CURRENT USER BOARD================="
     puts @user.user_board.render(true)
   end
->>>>>>> c44d2956acac6bbc07aafd1064d2be1632a72db0
 end
